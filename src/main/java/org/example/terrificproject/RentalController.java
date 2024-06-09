a6767837a7b484034ba2cd73843f22219821faa8
wlaszkiewicz <nika25.laszkiewicz@gmail.com>
2024-06-09 19:35:37 +0200

    szukanie update

--- src/main/java/org/example/terrificproject/RentalController.java
+++ src/main/java/org/example/terrificproject/RentalController.java
@@ -7,9 +7,8 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RentalController {
 
@@ -32,7 +31,7 @@ public class RentalController {
 
     private void addingVehicles(ArrayList<Vehicle> vehicles) {
         ArrayList<Date> rentalDates = new ArrayList<Date>(); // dla kazdego pojazdu bedzie inna  ale teraz jest tak
-        Vehicle vehicle1 = new Vehicle("2020", "Toyota", "Corolla", "Black", "Sedan", "ICE", rentalDates);
+        Vehicle vehicle1 = new Vehicle("2021", "Toyota", "Corolla", "Black", "Sedan", "ICE", rentalDates);
         Vehicle vehicle2 = new Vehicle("2020", "Miata", "Corolla", "Black", "Sedan", "Hybrid",rentalDates);
         Vehicle vehicle3 = new Vehicle("2020", "Toyota", "Corolla", "Black", "Sedan", "BEV",rentalDates);
         Vehicle vehicle4 = new Vehicle("2020", "Toyota", "Corolla", "Black", "Sedan", "ICE",rentalDates);
@@ -53,38 +52,44 @@ public class RentalController {
     void searchPressed(ActionEvent event) {
         vehiclesList.getItems().clear();
         String[] wordList = searchField.getText().split(" ");// podzielenie tekstu na slowa
+        System.out.println(wordList); // test
         HashMap<Vehicle, Integer> order = new HashMap<Vehicle, Integer>();
-        for(Vehicle vehicle : vehicles){
-            int wordsMatched = 0; // ilosc slow ktore pasuja do pojazdu
-            for(String word : wordList){
-                if(vehicle.getMake().contains(word)){
+        for (Vehicle vehicle : vehicles) {
+            int wordsMatched = 0; //
+            for (String word : wordList) {
+                if (vehicle.getMake().contains(word)) {
                     wordsMatched++;
                 }
-                if(vehicle.getModel().contains(word)){
+                if (vehicle.getModel().contains(word)) {
                     wordsMatched++;
                 }
-                if(vehicle.getYear().contains(word)){
+                if (vehicle.getYear().contains(word)) {
                     wordsMatched++;
                 }
-                if(vehicle.getColor().contains(word)){
+                if (vehicle.getColor().contains(word)) {
                     wordsMatched++;
                 }
-                if(vehicle.getType().contains(word)){
+                if (vehicle.getType().contains(word)) {
                     wordsMatched++;
                 }
-                if(vehicle.getPowertrain().contains(word)){
+                if (vehicle.getPowertrain().contains(word)) {
                     wordsMatched++;
                 }
             }
-            if(wordsMatched == 0) continue; // jesli nie pasuje zadne slowo to nie dodajemy do listy
+
+            if (wordsMatched == 0) continue; // if no words match do not add to the list
             order.put(vehicle, wordsMatched);
         }
-        order.entrySet().stream().sorted((k1, k2) -> -k2.getValue().compareTo(k1.getValue())); // sortowanie po ilosci slow ktore pasuja
-        for (Vehicle key : order.keySet()) { // dodanie pojazdow do listy w odpowiedniej kolejnosci
-            vehiclesList.getItems().add(new Text(key.toString())); // wyswietlenie pojazdow w odpowiedniej kolejnosci
-        }
 
+        List<Map.Entry<Vehicle, Integer>> sortedEntries = order.entrySet().stream()
+                .sorted(Map.Entry.<Vehicle, Integer>comparingByValue().reversed())
+                .toList(); // sort the vehicles by the number of words matched
+
+        for (Map.Entry<Vehicle, Integer> entry : sortedEntries) {
+            vehiclesList.getItems().add(new Text(entry.getKey().toString()));// display vehicles in the correct order
+        }
     }
+
 
 
 }
